6a6735696e0db78b451838e9379cb504add929a8
derekmhawkins <derekhawkins.tech@gmail.com>
2019-05-16 13:41:37 -0500

    All routes working. Navigation note fixed yet.

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,28 @@ from flask import render_template
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+  return render_template('portfolio.html')
+
+@app.route('/pricing')
+def pricing():
+  return render_template('pricing.html')
+
+@app.route('/blog')
+def blog():
+  return render_template('blog.html')
+
+@app.route('/blog-single')
+def blog_single():
+  return render_template('blog-single.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
