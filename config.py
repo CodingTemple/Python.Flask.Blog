53a1dc6c17dc5f829bc569e9639676e16d04407c
derekmhawkins <derekhawkins.tech@gmail.com>
2019-05-20 07:21:23 -0500

    config.py file

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
   SECRET_KEY = os.getenv('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or os.getenv('SQLALCHEMY_DATABASE_URI')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
